f0d72a639148c5709d44bb5a505b91a462b75bd1
AlmirNET <almir.neto369@gmail.com>
2022-08-28 14:50:25 -0300

    Difficulty squeezing javascript.
    Difficulty squeezing javascript.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-$(document).rady(function() {
+ $(document).rady(function() {
 
     var img1 = $('#img1').attr('src');
-    var img1 = $('#img1').attr('src');
-    var img1 = $('#img1').attr('src');
-    var img1 = $('#img1').attr('src');
+    var img2 = $('#img2').attr('src');
+    var img3 = $('#img3').attr('src');
+    var img4 = $('#img4').attr('src');
     $('#img1').click(function(){
-        $('preview').fade
+        $('preview').fadeOut(0);
+        $('preview').stop().fadeIn(400).attr('src',img1);
+    });
+    $('#img2').click(function(){
+        $('preview').fadeOut(0);
+        $('preview').stop().fadeIn(400).attr('src',img2);
+    });
+    $('#img3').click(function(){
+        $('preview').fadeOut(0);
+        $('preview').stop().fadeIn(400).attr('src',img3);
+    });
+    $('#img4').click(function(){
+        $('preview').fadeOut(0);
+        $('preview').stop().fadeIn(400).attr('src',img4);
     });
 });
