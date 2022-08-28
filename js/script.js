ef98626d0b82556c208610f7ba3c919daa366412
AlmirNET <almir.neto369@gmail.com>
2022-08-28 13:31:22 -0300

    Continuing javascript code.
    Continuing javascript code.

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ $(document).rady(function() {
     var img1 = $('#img1').attr('src');
     var img1 = $('#img1').attr('src');
     var img1 = $('#img1').attr('src');
+    var img1 = $('#img1').attr('src');
+    $('#img1').click(function(){
+        $('preview').fade
+    });
 });
